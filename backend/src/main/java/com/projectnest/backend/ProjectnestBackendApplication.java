ae15650f45bfb3bc93cee489a739e7a1f9e8093f
Jorge <ibarrapaezj@gmail.com>
2025-04-09 21:07:52 -0500

    Inicialización del proyecto

---
+++ backend/src/main/java/com/projectnest/backend/ProjectnestBackendApplication.java
@@ -0,0 +1,13 @@
+package com.projectnest.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectnestBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectnestBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/projectnest/backend/ProjectnestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectnest.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectnestBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
